14c5ad07071f583539efff5c48e71fed9a9c4519
anonymous anonymous <rsachs22@gmail.com>
2021-12-08 04:34:53 +0000

    Direct association between users and song_reviews

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Direct associations
 
+  has_many   :song_reviews,
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe User, type: :model do
   
     describe "Direct Associations" do
 
+    it { should have_many(:song_reviews) }
+
     end
 
     describe "InDirect Associations" do
